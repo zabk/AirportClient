96258dec710857d73e5e29378badc25e42bb581f
Krisztian Zab <zabk@wizzair.com>
2022-02-21 14:14:28 +0100

    Initial upload

--- AirportClient/MainWindow.xaml.cs
+++ AirportClient/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace AirportClient
 {
     /// <summary>
@@ -23,6 +24,31 @@ namespace AirportClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            AirportService.AirportServiceClient client = new AirportService.AirportServiceClient();
+            AirportService.Airport airport = new AirportService.Airport
+            {
+                AirportICAOCode =textBoxICAO.Text,
+                AirportIATACode=textBoxIATA.Text,
+                Minima=Convert.ToInt32(textBoxMinima.Text)
+            };
+            client.SaveAirport(airport);
+            textBoxICAO.Text = "";
+            textBoxIATA.Text = "";
+            textBoxMinima.Text = "";
+            statusBar.Text = "Airport saved";
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            AirportService.AirportServiceClient client = new AirportService.AirportServiceClient();
+            AirportService.Airport airport= client.GetAirport(textBoxICAO.Text);
+            textBoxIATA.Text = airport.AirportIATACode;
+            textBoxMinima.Text = airport.Minima.ToString();
+            statusBar.Text = "Airport loaded";
         }
     }
 }
